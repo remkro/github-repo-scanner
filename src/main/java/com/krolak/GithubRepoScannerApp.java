8e8bd3e5ede6474e388fc09b6bb26929a8db830b
remkro <krolak.remek@gmail.com>
2024-07-17 18:17:45 +0200

    project setup

---
+++ src/main/java/com/krolak/GithubRepoScannerApp.java
@@ -0,0 +1,11 @@
+package com.krolak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepoScannerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepoScannerApp.class, args);
+    }
+}
